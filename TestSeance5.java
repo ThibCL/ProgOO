8a780e95a666ac75aee31ba185eec6c08b2343d9
Thibault <thibault.cabourg@gmail.com>
2018-10-10 22:07:25 +0200

    Modification du test en rajoutant une creation dun joueure

--- TestSeance5.java
+++ TestSeance5.java
@@ -10,19 +10,22 @@ public class TestSeance5 {
         World w5= new World();
         w5.creationJoueur();
         w5.creationJoueur();
+        w5.creationJoueur();
         
         w5.creeMondeAlea();
         
         w5.getlJoueur().get(0).getPerso().affiche();
         w5.getlJoueur().get(1).getPerso().affiche();
+        w5.getlJoueur().get(2).getPerso().affiche();
         w5.affichemat();
         w5.getlJoueur().get(0).deplaceperso(w5);
-        w5.getlJoueur().get(1).getPerso().affiche();
+        w5.getlJoueur().get(1).deplaceperso(w5);
         w5.affichemat();
         
         w5.getlJoueur().get(0).getPerso().affiche();
         w5.tourDeJeu();
         w5.getlJoueur().get(0).getPerso().affiche();
+        
         
         
     }
