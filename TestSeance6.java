bd4e40ab05f02801373a424ac6e0e194596c170d
Thibault <thibault.cabourg@gmail.com>
2018-10-26 14:02:57 +0200

    ff

--- TestSeance6.java
+++ TestSeance6.java
@@ -10,14 +10,14 @@ import java.util.Scanner;
 public class TestSeance6 {
     public static void main(String[] args) throws IOException{
         
-       /* Partie p = new Partie();
+        Partie p = new Partie();
         SauvegardePartie testi =p.lancerPartie();
         p.getW().tourDeJeu(testi);/*
         World w=new World(5,10,10);
         w.creeMondeAlea();
         w.afficheMat();*/
        
-       //nuage qui n'est pas sur un objet
+       /*//nuage qui n'est pas sur un objet
         NuageToxique n1 = new NuageToxique("NuageToxique 0 3 9 67 0");
         n1.affiche();
        
@@ -25,7 +25,7 @@ public class TestSeance6 {
         
         //nuage placé sur un objet de type Soin
         NuageToxique n2 = new NuageToxique("NuageToxique 6 9 9 70 1 [Soin 9 6 16]");
-        n2.affiche();
+        n2.affiche();*/
         
     }   
 }
